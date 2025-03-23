5c808146f75902f13aaf2dc8f1de872028c4131b
artursbelskis <144722286+artursbelskis@users.noreply.github.com>
2025-03-23 23:38:02 +0000

    Izveidota Heiristiska funkcija

--- ai/heuristic.py
+++ ai/heuristic.py
@@ -1,2 +1,12 @@
-def evaluate():
-    pass
+def evaluate(state):
+    # aprēķinām punktu starpību
+    score_difference = state.computer_points - state.human_points 
+    # atrast pašreizējo gājiena numuru, dalot ar 10, var samazināt šī skaitļa ietekmi
+    number_factor = state.current_number / 10
+
+    # tiek atgriezta fiksēta vērtība: 10000 - ja dators uzvarēja, -10000 - ja uzvarējis cilvēks -- Šī vērtība norāda spēles beigas
+    if state.is_game_over:
+        return 10000 if state.computer_points > state.human_points else -10000
+
+    # Šī summa būs pašreizējā spēles stāvokļa novērtējums, lai pieņemtu lēmumus spēles stratēģijā (piemēram, minimax meklēšanas algoritma gadījumā)
+    return score_difference + number_factor

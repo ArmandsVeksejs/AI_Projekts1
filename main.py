c633ebd10b8cb38c3a2cf32e72bf3bea14c292c1
Andrejs <andrejs.leja@edu.rtu.lv>
2025-03-26 16:03:42 +0200

    Izveidoju alpha_beta kodu kurs skatas kurs gajiens bus labaks prieks AI kurs apskata bernus no koka un izvelas labako gajienu. Lai redzet kurš gajiens ir labak priekš AI main.py pievienoju best_move un izvadu vinu. Tas radis apskatamo skaitli cilveka un AI punktus un dzilumu. Un raksta kurs tas ir gajiens par to ir labak uzrakstits komentara virs print(best_move)

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from game import GameState, print_game_tree
+from algoritmi import get_best_move,heuristic
 
 def main():
+
+    
     initial_state = GameState(number=7)
     max_depth = 3
+
     print("Spēles koks:")
     print_game_tree(initial_state, max_depth)
+    
+    best_move = get_best_move(initial_state, max_depth)
+    print("\nLabākais gājiens, ko izvēlas AI (ar alpha-beta):")
+    #Vai cilvēka gājiens=False (tas nozime ka šis gājiens noved pie AI priekšrocības nākotnē)
+    print(best_move)
 
 if __name__ == "__main__":
     main()
